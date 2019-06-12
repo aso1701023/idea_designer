9850d684405cc3cd7b8ecfe09592c8ea057df746
aso1701011 <1701011@st.asojuku.ac.jp>
2019-06-12 10:48:32 +0900

    var0

---
+++ app/src/main/java/jp/ac/asojuku/st/idea_designer/NoteItemActivity.kt
@@ -0,0 +1,12 @@
+package jp.ac.asojuku.st.idea_designer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_item)
+    }
+}
